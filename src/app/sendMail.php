944405d24efc911bc10f339890d6e4f9df9a48ca
Maik Wypior <160053500+MaikWpr83@users.noreply.github.com>
2025-09-23 21:07:24 +0200

    first commit

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'maikwypior83@outlook.com';
+            $recipient = 'maik.wypior@yahoo.com';
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
