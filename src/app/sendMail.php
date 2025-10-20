ed9e007c407e322d6cb6d56fb4f1f9dcb4525966
Maik Wypior <160053500+MaikWpr83@users.noreply.github.com>
2025-10-20 20:26:52 +0200

    Change email recipient in sendMail.php

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'maik.wypior@yahoo.com';
+            $recipient = 'kontakt@maik.wypior.de';
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
