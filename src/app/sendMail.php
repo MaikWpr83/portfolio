67bda7219fb52c11e66c18f232891a948bc9c6c0
Maik Wypior <160053500+MaikWpr83@users.noreply.github.com>
2025-11-02 14:18:32 +0100

    Fix email recipient address format

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'kontakt@maik.wypior.de';
+            $recipient = 'kontakt@maik-wypior.de';
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
